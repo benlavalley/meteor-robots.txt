4e1b668f0473d1537aa373b68fb6df6ca0eb7ad7
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 01:24:54 -0400

    version bump

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  summary: "Serves a robot.txt which can be modified programatically",
-  version: "0.0.10",
-  git: "https://github.com/gadicc/meteor-robots.txt.git"
+	summary: 'Serves a robot.txt which can be modified programatically',
+	version: '0.0.11',
+	git: 'https://github.com/gadicc/meteor-robots.txt.git',
 });
 
 Package.on_use(function (api) {
-  api.use('webapp@1.0.0', 'server');
-	api.add_files('robots.js', 'server');
+	api.use('webapp@1.8.1', 'server');
+	api.addFiles('robots.js', 'server');
 	api.export('robots', 'server');
 });
