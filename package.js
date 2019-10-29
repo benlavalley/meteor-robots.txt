e7a7ef890af12a20e548f027f5325f5d00c36542
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 01:29:28 -0400

    add package name

--- package.js
+++ package.js
@@ -1,4 +1,5 @@
 Package.describe({
+	name: 'gadicohen:robots-txt',
 	summary: 'Serves a robot.txt which can be modified programatically',
 	version: '0.0.11',
 	git: 'https://github.com/gadicc/meteor-robots.txt.git',
